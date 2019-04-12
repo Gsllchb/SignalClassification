631f1d04d15379a655f07355b9397d44b6112a9e
Gsllchb <gsllchb@gmail.com>
2019-04-12 16:59:41 +0800

    improve figs

--- Insights/plot_distributions.py
+++ Insights/plot_distributions.py
@@ -2,8 +2,7 @@
 """分别作信号和噪音数(密度)在沉积能量、漂移时间、半径及phi上的分布图"""
 import matplotlib.pyplot as plt
 import pandas as pd
-
-import dotdot
+import numpy as np
 from constants import *
 
 BIN_NUM = 50
@@ -16,88 +15,90 @@ def main():
     signal_label = "signal"
     background_label = "background"
 
-    for density in (True, False):
-        y_label = "hit density" if density else "hit"
+    density = True
+    y_label = "hit density"
 
-        # energy distribution
-        plt.hist(
-            np.log2(signals.me),
-            BIN_NUM,
-            density=density,
-            alpha=0.5,
-            label=signal_label,
-        )
-        plt.hist(
-            np.log2(backgrounds.me),
-            BIN_NUM,
-            density=density,
-            alpha=0.5,
-            label=background_label,
-        )
-        plt.legend()
-        plt.xlabel("deposit energy (log2 scale)")
-        plt.ylabel(y_label)
-        plt.show()
+    # energy distribution
+    plt.hist(
+        np.log2(signals.me),
+        BIN_NUM,
+        density=density,
+        alpha=0.5,
+        label=signal_label,
+    )
+    plt.hist(
+        np.log2(backgrounds.me),
+        BIN_NUM,
+        density=density,
+        alpha=0.5,
+        label=background_label,
+    )
+    plt.legend()
+    plt.xlabel("deposit energy (log2 scale)")
+    plt.ylabel(y_label)
+    plt.show()
 
-        # drift time distribution
-        plt.hist(
-            signals.mdetmt0,
-            BIN_NUM,
-            density=density,
-            alpha=0.5,
-            label=signal_label,
-        )
-        plt.hist(
-            backgrounds.mdetmt0,
-            BIN_NUM,
-            density=density,
-            alpha=0.5,
-            label=background_label,
-        )
-        plt.legend()
-        plt.xlabel("drift time")
-        plt.ylabel(y_label)
-        plt.show()
+    # drift time distribution
+    plt.hist(
+        signals.mdetmt0,
+        BIN_NUM,
+        density=density,
+        alpha=0.5,
+        label=signal_label,
+    )
+    plt.hist(
+        backgrounds.mdetmt0,
+        BIN_NUM,
+        density=density,
+        alpha=0.5,
+        label=background_label,
+    )
+    plt.legend()
+    plt.xticks(range(-100, 1201, 200))
+    plt.xlabel("drift time")
+    plt.ylabel(y_label)
+    plt.show()
 
-        # layer distribution
-        plt.hist(
-            signals.layer,
-            N_LAYERS,
-            density=density,
-            alpha=0.5,
-            label=signal_label,
-        )
-        plt.hist(
-            backgrounds.layer,
-            N_LAYERS,
-            density=density,
-            alpha=0.5,
-            label=background_label,
-        )
-        plt.legend()
-        plt.xlabel("layer")
-        plt.ylabel(y_label)
-        plt.show()
+    # layer distribution
+    plt.hist(
+        signals.layer,
+        N_LAYERS,
+        density=density,
+        alpha=0.5,
+        label=signal_label,
+    )
+    plt.hist(
+        backgrounds.layer,
+        N_LAYERS,
+        density=density,
+        alpha=0.5,
+        label=background_label,
+    )
+    plt.legend()
+    plt.xticks(np.arange(0, 18, 1))
+    plt.xlabel("layer")
+    plt.ylabel(y_label)
+    plt.show()
 
-        # phi distribution
-        plt.hist(
-            signals.phi,
-            BIN_NUM,
-            density=density,
-            alpha=0.5,
-            label=signal_label,
-        )
-        plt.hist(
-            backgrounds.phi,
-            BIN_NUM,
-            density=density,
-            alpha=0.5,
-            label=background_label,
-        )
-        plt.legend()
-        plt.xlabel("phi")
-        plt.ylabel(y_label)
-        plt.show()
+    # phi distribution
+    plt.hist(
+        signals.phi,
+        BIN_NUM,
+        density=density,
+        alpha=0.5,
+        label=signal_label,
+    )
+    plt.hist(
+        backgrounds.phi,
+        BIN_NUM,
+        density=density,
+        alpha=0.5,
+        label=background_label,
+    )
+    plt.legend()
+    plt.xlabel("phi")
+    plt.ylabel(y_label)
+    plt.show()
 
 
 if __name__ == '__main__':
